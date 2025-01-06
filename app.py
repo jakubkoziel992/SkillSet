4276912316019b503ceb3678c6362a50584c69aa
Koziel <jakub.koziel@accenture.com>
2025-01-06 16:42:01 +0100

    new methods have been implemented

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from database import db
 from models import Course, Author
-from schemas import ma, courses_schema, author_schema, authors_schema
+from schemas import ma, CourseSchema, AuthorSchema
 
 
 #Init app
@@ -23,15 +23,78 @@ def health_check():
 
 @app.route('/course', methods=['GET'])
 def get_courses():
+    courses_schema = CourseSchema(many=True)
     courses = Course.query.all()  
     
     return jsonify(courses_schema.dump(courses))
 
+@app.route('/course', methods=['POST'])
+def add_course():
+    course_input_schema = CourseSchema()
+    
+    data = request.json
+    
+    name = data['name']
+    author_id = data['author_id']
+    platform = data['platform']
+    completion_date = data['completion_date']
+
+    author = Author.query.filter_by(id=author_id).first()
+    
+    if author:
+        course = Course(name, author_id, platform, completion_date)
+        db.session.add(course)
+        db.session.commit()
+        
+        return jsonify(course_input_schema.dump(course))
+    
+    return jsonify({"error": f"autor nie został znaleziony"}), 404
+
+@app.route('/course/<int:id>/', methods=['DELETE'])
+def delete_course(id):
+    course_schema = CourseSchema()
+    course = Course.query.get(id)
+    
+    db.session.delete(course)
+    db.session.commit()
+    
+    return jsonify(course_schema.dump(course))
+    
+         
+
 @app.route('/author', methods=['GET'])
 def get_authors():
+    authors_schema = AuthorSchema(many=True)
     authors = Author.query.all()
     
     return jsonify(authors_schema.dump(authors))
- 
+
+@app.route('/author', methods=['POST'])
+def add_author():
+    author_input_schema = AuthorSchema()
+    
+    name = request.json['name']
+    
+    author = Author.query.filter_by(name=name).first()
+    
+    if not author:
+        new_author = Author(name)
+        db.session.add(new_author)
+        db.session.commit()
+        
+        return jsonify(author_input_schema.dump(new_author))
+
+    return jsonify({"error": f"Istnieje już autor o nazwe {author}"}), 404
+
+@app.route('/author/<int:id>/', methods=['DELETE'])
+def delete_author(id):
+    author_schema = AuthorSchema()
+    author = Author.query.get(id)
+    
+    db.session.delete(author)
+    db.session.commit()
+    
+    return jsonify(author_schema.dump(author))
+    
 if __name__ == "__main__":
     app.run(debug=True)
