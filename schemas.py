2d7184b311436b3248775ace4db6bf0fcd8f9844
Koziel <jakub.koziel@accenture.com>
2025-01-06 16:40:33 +0100

    changed value of relationship variable

--- schemas.py
+++ schemas.py
@@ -7,19 +7,13 @@ class AuthorSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name')
     
-    courses = fields.Nested('CourseSchema', many=True)
+    # courses = fields.Pluck('CourseSchema', 'name', many=True)
+    courses = fields.Function(lambda obj: [course.name for course in obj.courses])
+
 
 class CourseSchema(ma.Schema):
     class Meta:
         fields = ('id', 'name', 'author_id', 'platform', 'completion_date')
     
-    author = fields.Nested(AuthorSchema)
-
-
-
-
-course_schema = CourseSchema()
-courses_schema = CourseSchema(many=True)
-
-author_schema = AuthorSchema()
-authors_schema = AuthorSchema(many=True)
+    # author_name = fields.Nested(AuthorSchema)
+    author_name = fields.Function(lambda obj: obj.author.name if obj.author else None)
