b402fbf1f3dbac51b7fd5ae6b1e2d92b91b0f227
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-01-15 08:03:34 +0100

    created conf classes for particular environment

--- config.py
+++ config.py
@@ -1,5 +1,29 @@
 import os
 
 class Config: 
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:admin@127.0.0.1/skill_set')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    
+    if SQLALCHEMY_DATABASE_URI is None:
+        SQLALCHEMY_DATABASE_URI = (
+            "mysql+mysqlconnector://"
+            + os.environ.get("DB_USER", "")
+            + ":"
+            + os.environ.get("DB_PASSWORD", "")
+            + "@"
+            + os.environ.get("DB_HOST", "")
+            + "/"
+            + os.environ.get("DB_NAME", "")
+        )
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql+mysqlconnector://root:admin@127.0.0.1/skill_set')
+    
+    
+# config_dict = {
+#     "dev" : DevelopmentConfig,
+#     "prod" : ProductionConfig
+# }
