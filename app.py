34965a62bae03cbf86c3ee5de399478e68ddfd9e
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-01-16 14:01:15 +0100

    added secret key for CSRF token

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ else:
 app = Flask(__name__)
 
 app.config.from_object(config)
-
+app.config['SECRET_KEY'] = SECRET_KEY
 
 db.init_app(app)
 ma.init_app(app)
@@ -25,4 +25,6 @@ ma.init_app(app)
 app.register_blueprint(blueprint)
  
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0')
