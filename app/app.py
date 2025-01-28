fae06e0255e919a0add604a429c37e64671d257f
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-01-28 08:22:49 +0100

    created conf for testing puproses

--- app/app.py
+++ app/app.py
@@ -3,28 +3,33 @@ from database import db
 from config import DevelopmentConfig, ProductionConfig
 from routes import blueprint
 from schemas import ma
-from os import environ, getenv
-
-# env = getenv("FLASK_ENV", "dev")
-env = environ.get("FLASK_ENV", "dev")
-
-if env == "prod":
-    config = ProductionConfig()
-else:
-    config = DevelopmentConfig()
+from os import environ,getenv
 
 
-app = Flask(__name__)
+def create_app(config_name=None):
+    app = Flask(__name__)
 
-app.config.from_object(config)
-app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+    if not config_name:
+        env = environ.get("FLASK_ENV", "dev")
+        if env == "prod":
+            config_name = ProductionConfig()
+        elif env == "test":
+            config_name = TestingConfig()
+        else:
+            config_name = DevelopmentConfig()
 
-db.init_app(app)
-ma.init_app(app)
+    app.config.from_object(config_name)
+    app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
-app.register_blueprint(blueprint)
- 
+    db.init_app(app)
+    ma.init_app(app)
+
+    app.register_blueprint(blueprint)
+
+    return app
+
 if __name__ == "__main__":
+    app = create_app()
     with app.app_context():
         db.create_all()
     app.run(host='0.0.0.0')
