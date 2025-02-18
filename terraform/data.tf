08f6cf537b63b9d6a28d5d87ad3ea6398604c1b1
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-02-18 07:53:58 +0100

    captured ubuntu image

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,3 +7,19 @@ data "aws_vpc" "default" {
 data "http" "myip" {
   url = "https://ifconfig.io"
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners = ["amazon"]
+  #image_owner_alias = "amazon"
+
+  filter {
+    name  = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
