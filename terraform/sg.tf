d74b05669df537decff8271dd9e44f6558959a97
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-02-24 18:52:20 +0100

    changed port number

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -17,13 +17,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_to_port_8000" {
-  description       = "open port 8000"
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_to_port_80" {
+  description       = "open port 80"
   security_group_id = aws_security_group.skillset-web-SG.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 8000
+  from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = 8000
+  to_port           = 80
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
