fb6b794d01cf15bf13f68ee5343d63f533ef3d56
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-02-24 17:50:28 +0100

    updated sg

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -17,13 +17,13 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_to_port_5000" {
-  description       = "open port 5000"
+resource "aws_vpc_security_group_ingress_rule" "allow_all_traffic_to_port_8000" {
+  description       = "open port 8000"
   security_group_id = aws_security_group.skillset-web-SG.id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 5000
+  from_port         = 8000
   ip_protocol       = "tcp"
-  to_port           = 5000
+  to_port           = 8000
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
