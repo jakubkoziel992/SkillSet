ff854e6987c7aebb6bc3a1c1a7a1392b63bf2e89
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-02-25 12:52:34 +0100

    created log function

--- init_app.sh
+++ init_app.sh
@@ -8,53 +8,63 @@ export DB_HOST="${db_host}"
 export DB_NAME="${db_name}"
 
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
-export LOG_FILE=/home/ubuntu/skillset_init_$TIMESTAMP.log
+export LOG_FILE=/var/log/skillset_init_$TIMESTAMP.log
+
+function log {
+	if [[ -n $1 ]]; then
+		echo "$1" | tee -a "$LOG_FILE"
+		return 0
+	fi
+        while read -r line; do
+                echo "$line" | tee -a "$LOG_FILE"
+        done < "/dev/stdin"
+}
+
+if [[ ! -f $LOG_FILE ]]; then
+    log "Creating log_file"
+    sudo mkdir -m 600 $LOG_FILE | log
+fi
 
 cd /home/ubuntu
 
-echo "Installing the required applications...." > $LOG_FILE 2>&1
+log "Installing the required applications...."
 sudo apt update -y
-sudo apt install nginx-extras -y
-sudo apt install python3 -y && sudo apt install python3-pip -y
+sudo apt update -y && sudo apt install -y nginx-extras python3 python3-pip
 
-[[ $? -eq 0 ]] && echo "Succesfully installed python and nginx" >> $LOG_FILE 2>&1
+[[ $? -eq 0 ]] && log "Succesfully installed python and nginx"
 
 
 
 
 #clone repository
-git clone https://github.com/jakubkoziel992/SkillSet.git && echo "Repository copied succesfully" >> $LOG_FILE 2>&1
+git clone https://github.com/jakubkoziel992/SkillSet.git | log
 
 cd ./SkillSet
 #checkout on branch
-git checkout aws-terraform
+git checkout aws-terraform | log
 
-sudo cp ./nginx/default.conf /etc/nginx/sites-available/default && echo "Copied nginx configuration" >> $LOG_FILE 2>&1
+sudo cp ./nginx/default.conf /etc/nginx/sites-available/default && log "Copied nginx configuration"
 sudo nginx -s reload
-#cd /etc/nginx/sites-enabled
-#sudo ln -s /etc/nginx/sites-available/default
-
-
 
 #install requirements
 cd ./app
-pip install -r ./requirements.txt && echo "Installed python requirements" >> $LOG_FILE 2>&1
-
+pip install -r ./requirements.txt && log "Installed python requirements"
 
 
 #start application
-echo "Starting application" >> $LOG_FILE 2>&1
-#gunicorn -c ../gunicorn.conf.py &> /home/ubuntu/gunicorn.log &
-gunicorn -c ../gunicorn.conf.py  >> /dev/null 2>&1 &
+log "Starting application"
+gunicorn -c ../gunicorn.conf.py | log
 
-if pgrep -x "gunicorn" >> $LOG_FILE; then
-    echo "Application is working" >> $LOG_FILE
+if pgrep -x "gunicorn" > /dev/null 2>&1; then
+    log "Application is working"
     exit 0
 else
-    echo "Application not working" >> $LOG_FILE
-    echo "Restarting application" >> $LOG_FILE
-    gunicorn -c ../gunicorn.conf.py  >> $LOG_FILE 2>&1 &
+    log "Application not working."
+    log "Restarting application."
+    gunicorn -c ../gunicorn.conf.py  | log
 fi
+
+log "Init script finished succesfully."
 
 
 
