657f25745ed974a61776ec05f01b4d5902e0a75f
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-02-27 17:02:11 +0100

    moved input variables

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,17 @@
+db_name = "skillset"
+engine =  "mysql"
+engine_version = "8.0.40"
+instance_class = "db.t3.micro"
+allocated_storage = 10
+instance_identifier = "skillset-db"
+parameter_group_name = "default.mysql8.0"
+availability_zone = "us-east-1a"
+instance_type = "t2.micro"
+volume_size = "20"
+key_algorithm = "RSA"
+key_name = "flask-app"
+flask_app = "prod"
+
 ingress_rules = {
   "allow-ssh" = {
     description = "Allow ssh"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,84 +7,83 @@ variable "password" {
   sensitive = true
 }
 
-#db
 variable "db_name" {
   description = "Database name"
   type        = string
-  default     = "skillset"
+  #default     = "skillset"
 }
 
 variable "engine" {
   description = "Database engine"
   type        = string
-  default     = "mysql"
+  #default     = "mysql"
 }
 
 variable "engine_version" {
   description = "Database engine version"
   type        = string
-  default     = "8.0.40"
+  #default     = "8.0.40"
 }
 
 variable "instance_class" {
   description = "Database instance class"
   type        = string
-  default     = "db.t3.micro"
+  #default     = "db.t3.micro"
 }
 
 variable "allocated_storage" {
   description = "Allocated storage for the DB"
   type        = number
-  default     = 10
+  #default     = 10
 }
 
 variable "instance_identifier" {
   description = "The DB instance identifier"
   type        = string
-  default     = "skillset-db"
+  #default     = "skillset-db"
 }
 
 variable "parameter_group_name" {
   description = "value"
   type        = string
-  default     = "default.mysql8.0"
+  #default     = "default.mysql8.0"
 }
 
 variable "availability_zone" {
   description = "Ec2 instance availability zone"
   type        = string
-  default     = "us-east-1a"
+  #default     = "us-east-1a"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t2.micro"
+  #default     = "t2.micro"
 }
 
 variable "flask_app" {
   description = "FLASK_APP value"
   type        = string
-  default     = "prod"
+  #default     = "prod"
 }
 
 
 variable "volume_size" {
   description = "Device volume size"
   type        = string
-  default     = "20"
+  #default     = "20"
 }
 
 variable "key_algorithm" {
   description = "SSH key algorithm"
   type        = string
-  default     = "RSA"
+  #default     = "RSA"
 }
 
 variable "key_name" {
   description = "SSH key name"
   type        = string
-  default     = "flask-app"
+  #default     = "flask-app"
 }
 
 variable "ingress_rules" {
