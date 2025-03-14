2368a1fe502196b9102cf128c517620178338e4e
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-03-14 09:19:27 +0100

    launched sg module

--- terraform/envs/prod/app/data.tf
+++ terraform/envs/prod/app/data.tf
@@ -12,6 +12,6 @@ data "http" "myip" {
 data "terraform_remote_state" "vpc" {
   backend = "local"
   config = {
-    path = "./vpc/terraform.tfstate"
+    path = "../vpc/terraform.tfstate"
   }
 }
--- terraform/envs/prod/app/main.tf
+++ terraform/envs/prod/app/main.tf
@@ -1,5 +1,13 @@
+module "sg" {
+  source               = "../../../modules/sg"
+  vpc_id               = data.aws_vpc.vpc.id
+  ingress_rules        = var.ingress_rules
+  DB_ingress_rules     = var.DB_ingress_rules
+  ec2_ip               = chomp(data.http.myip.response_body)
+}
+
 module "rds" {
-  source               = "./modules/rds-mysql"
+  source               = "../../../modules/rds-mysql"
   username             = var.username
   password             = var.password
   db_name              = var.db_name
@@ -10,16 +18,15 @@ module "rds" {
   allocated_storage    = var.allocated_storage
   parameter_group_name = var.parameter_group_name
   vpc_id               = data.aws_vpc.vpc.id
-  ec2_SG               = module.ec2.ec2_SG
   snapshot             = var.snapshot
   public_access        = var.public_access
   multi_az             = var.multi_az
-  DB_ingress_rules     = var.DB_ingress_rules
   private_subnets      = data.terraform_remote_state.vpc.outputs.private_subnets
+  rds_sg               = module.sg.rds_sg 
 }
 
-module "ec2" {
-  source                 = "./modules/ec2"
+module "ec2-1" {
+  source                 = "../../../modules/ec2"
   username               = var.username
   password               = var.password
   availability_zone      = var.availability_zone
@@ -31,11 +38,31 @@ module "ec2" {
   private_key_permission = var.private_key_permission
   key_path               = var.key_path
   flask_app              = var.flask_app
-  ingress_rules          = var.ingress_rules
   db_name                = var.db_name
   vpc_id                 = data.aws_vpc.vpc.id
   db_host                = module.rds.mysql_host
-  ec2_ip                 = chomp(data.http.myip.response_body)
   app_secret_key         = var.app_secret_key
-  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnet
+  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[0]
+  ec2_sg                 = module.sg.ec2_sg 
+}
+
+module "ec2-2" {
+  source                 = "../../../modules/ec2"
+  username               = var.username
+  password               = var.password
+  availability_zone      = "us-east-1b"
+  instance_type          = var.instance_type
+  instance_name          = "skillset-web-2"
+  volume_size            = var.volume_size
+  key_algorithm          = var.key_algorithm
+  key_name               = "flask-app-2"
+  private_key_permission = var.private_key_permission
+  key_path               = "C:\\Users\\jakub.koziel\\Downloads/flask-app2.pem"
+  flask_app              = var.flask_app
+  db_name                = var.db_name
+  vpc_id                 = data.aws_vpc.vpc.id
+  db_host                = module.rds.mysql_host
+  app_secret_key         = var.app_secret_key
+  subnet_id              = data.terraform_remote_state.vpc.outputs.public_subnets[1]
+  ec2_sg                 = module.sg.ec2_sg 
 }
