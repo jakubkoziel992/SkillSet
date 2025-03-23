515b6bdf7717110da6a4bfc48086f2759a963379
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-03-23 16:34:54 +0100

    launched ecs module on dev environment

---
+++ terraform/envs/dev/ecs/main.tf
@@ -0,0 +1,20 @@
+module "alb" {
+  source           = "../../../modules/loadbalancer"
+  name             = "skillset"
+  lb_type          = "application"
+  target_type      = "ip" 
+  vpc_id           =  data.aws_vpc.default.id
+  public_subnets   = data.aws_subnets.default_subnets.ids
+  ec2_instance_ids = []
+
+}
+
+
+module "ecs" {
+  source = "../../../modules/ecs"
+  service_subnets = data.aws_subnets.default_subnets.ids
+  task_definitions = var.task_definitions
+  service_definitions = var.service_definitions
+  target_group_arn = module.alb.target_group_arn
+  security_group_id = module.alb.security_group_id
+}
---
+++ terraform/envs/dev/ecs/variables.tf
@@ -0,0 +1,64 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "skillset" 
+}
+
+variable "execution_role" {
+    description = "execution role ARN"
+    type    = string 
+    default = "arn:aws:iam::320183397498:role/ecsTaskExecutionRole"
+}
+
+variable "service_subnets" {
+  description = "Id subnets"
+  type        = list(string) 
+}
+
+variable "task_definitions"{
+  description = "Task details"
+  type        = map(object({
+    name    = string
+    image   = string
+    cpu     = number
+    memory  = number
+    port_mapping = list(object({
+        name = string
+        containerPort = number
+        hostPort = number
+        protocol = string
+        appProtocol = optional(string)
+    }))
+    env_variables = list(object({
+      name = string
+      value = string 
+    }))
+    # health_check = optional(object({
+    #   command    = list(string)
+    #   interval   = number
+    #   timeout    = number
+    #   retries    = number
+    #   startPeriod = number
+    # }))
+  })) 
+}
+
+
+variable "service_definitions" {
+  description = "Service details"
+  type         = map(object({
+    service_name = string
+    desired_count = number
+    service = optional(object({
+      port_name = string
+      client_alias = object({
+        dns_name = string
+        port = number
+    })
+  }))
+    load_balancer = optional(object({
+      container_name = string
+      container_port = number
+    }))
+  }))
+} 
