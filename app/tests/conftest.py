bf5761616753d620419a7121f89ae45a02457c9a
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-04-06 17:32:12 +0000

    feat(ci-cd): create initial pipeline

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,6 +1,6 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 import pytest
 from app import create_app
