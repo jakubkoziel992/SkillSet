6ad781a86d377f72701216aad7af83cd3e5f0a17
Jakub Kozieł <jakubkoziel992@gmail.com>
2025-04-07 17:31:47 +0000

    feat(docker): change sleep time (#9)

--- app/wait_for_db.sh
+++ app/wait_for_db.sh
@@ -6,7 +6,7 @@ DB_PORT=3306
 #Check if mysql has started
 while ! nc -zv $DB_HOST $DB_PORT > /dev/null 2>&1 ;
 do
-  echo "Waiting for MYSQL Database Startup" && sleep 15
+  echo "Waiting for MYSQL Database Startup." && sleep 15
 done
 
 
